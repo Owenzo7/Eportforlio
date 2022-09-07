c1b91663cd6feab7c5b74c1b7f0f7863fce6e7b5
Owen Lee <owenlee545@gmail.com>
2022-09-07 17:29:07 +0300

    made some major changes

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-// template_pau2pyr
+// 
 
-// service_o3zsd3m - Service ID
+
 
 
 // xjhoIWRGWwVnvUg96 - Public Key
+
+
+function contact (event) {
+
+    event.preventDefault()
+
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+    .sendForm(
+        'service_o3zsd3m',
+        'template_pau2pyr',
+        event.target,
+        'xjhoIWRGWwVnvUg96'
+    ).then(()=> {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        console.log('it worked');
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The Email Servers are temporarily unavailable. Please contact me directly on owenrogers545@gmail.com"
+        
+            )
+
+    })    
+}
+
+let isModalOpen = false;
+function toggleModal () {
+
+    
+
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    
+    }
+        
+        
+
+    isModalOpen = true;
+   document.body.classList += " modal--open"
+}
+
+
+
